ce5c1473e54c3971df81cd068269327a7a84b938
atiQkhalid <39992488+atiQkhalid@users.noreply.github.com>
2021-12-27 05:49:29 +0500

    permissions

--- app/src/main/java/com/example/practice/views/fragments/cameramodule/CameraFragment.kt
+++ app/src/main/java/com/example/practice/views/fragments/cameramodule/CameraFragment.kt
@@ -1,15 +1,21 @@
 package com.example.practice.views.fragments.cameramodule
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity.RESULT_OK
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.PermissionChecker
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import com.example.practice.R
 import com.example.practice.base.BaseFragment
 import com.example.practice.databinding.FragmentCameraBinding
@@ -23,8 +29,6 @@ class CameraFragment : BaseFragment(), View.OnClickListener {
 
     private lateinit var binding: FragmentCameraBinding
     private lateinit var cameraAdapter: CameraAdapter
-    private val REQUEST_IMAGE_CAPTURE = 1
-    private val SELECT_PICTURE = 2
     var uri: ArrayList<Uri> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,17 +48,17 @@ class CameraFragment : BaseFragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.tvTakePicture.setOnClickListener(this)
-//        binding.ivClickedPicture.setOnClickListener(this)
         binding.tvSelectPictureFromGallery.setOnClickListener(this)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d("data scheme", data?.scheme.toString())
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as Bitmap
             binding.ivClickedPicture.visible()
             binding.ivClickedPicture.setImageBitmap(imageBitmap)
-        } else if (resultCode == RESULT_OK && requestCode == SELECT_PICTURE) {
+        } else if (resultCode == RESULT_OK && requestCode == SELECT_ITEM) {
             if (data?.data == null) {
                 uri.clear()
                 binding.ivClickedPicture.gone()
@@ -76,11 +80,24 @@ class CameraFragment : BaseFragment(), View.OnClickListener {
 
     // open Gallery
     private fun openGallery() {
+        if (checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PermissionChecker.PERMISSION_GRANTED){
+            val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+            requestPermissions(permissions, PERMISSION_CODE)
+        }
+        else {
+            chooseImageGallery()
+        }
+    }
+
+    private fun chooseImageGallery(){
         val intent = Intent()
-        intent.type = "image/*"
+        intent.type = "image/* video/*"
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
         intent.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE)
+        startActivityForResult(
+            Intent.createChooser(intent, "Select item from gallery"),
+            SELECT_ITEM
+        )
     }
 
     //  open camera
@@ -94,10 +111,32 @@ class CameraFragment : BaseFragment(), View.OnClickListener {
         }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    chooseImageGallery()
+                }else{
+                    showToastMsg("Permission Denied")
+                }
+            }
+        }
+    }
+
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.tv_takePicture -> openCamera()
             R.id.tv_SelectPictureFromGallery -> openGallery()
         }
+    }
+
+    companion object{
+        private const val REQUEST_IMAGE_CAPTURE = 1001
+        private const val SELECT_ITEM = 1002
+        private const val PERMISSION_CODE = 1003
     }
 }
